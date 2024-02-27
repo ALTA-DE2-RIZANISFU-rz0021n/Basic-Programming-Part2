3c6e49ac868df674db3c11fa07f747a2b6f81d94
rizanisfu <rizansf21@gmail.com>
2024-02-27 17:53:29 +0700

    updated task

--- problem2/main.py
+++ problem2/main.py
@@ -4,3 +4,12 @@ tinggi = 25.0
 
 
 # kode disini
+print('menghitung luas segitiga')
+print('------------------------')
+
+alas = float(input('masukkan nilai alas : '))
+tinggi = float(input('masukkan nilai tinggi : '))
+
+luas = 0.5 * alas * tinggi
+
+print('\nluasnya =', str(luas))
