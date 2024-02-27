cb9cd9d97687215dcf6e4778e189b33319b202cb
rizanisfu <rizansf21@gmail.com>
2024-02-27 22:37:34 +0700

    updated task

--- problem3/main.py
+++ problem3/main.py
@@ -4,3 +4,15 @@ r = 4.0
 
 
 # kode disini
+pi = 3.14
+jari_jari = 4.0
+tinggi =  20.0
+
+def hitung_luas_permukaan_tabung(jari_jari, tinggi):
+    Luas_permukaan = 2*pi*r*(r + t)
+
+r = float(input("Masukkan jari-jari tabung: "))
+t = float(input("Masukkan tinggi tabung: "))
+
+luas_permukaan = 2*pi*r*(r + t)
+print("Luas permukaan tabung adalah:", luas_permukaan)
