cb67890c3e2ed48217cc5085034494b54966294c
rizanisfu <rizansf21@gmail.com>
2024-03-04 13:16:17 +0700

    updated task

--- problem4/main.py
+++ problem4/main.py
@@ -3,3 +3,9 @@ Input Harga: 370000
 Input Diskon: 15
 Output: harga yang harus dibayar adalah Rp. 314.500
 '''
+
+harga_awal = int(input('masukkan harga produk: '))
+diskon = int(input('masukkan harga diskon: '))
+
+harga_akhir = int((100-diskon)/100*harga_awal)
+print('Harga setelah diskon: ', harga_akhir)
